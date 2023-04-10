1ac7b16730d9fa7a628a552e4cb949957102b140
Marcel Baur <marcel@baur.gg>
2023-04-10 16:07:18 +0200

    fix(tests): fix test case

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use propparse::fetch_file;
+
+fn main() {
+    println!("Hello");
+    println!("Hello");
+    match fetch_file("res/demo.properties") {
+        Ok(p) => {
+            println!("{:?}", p);
+        }
+        Err(_) => println!("Error"),
+    };
+}
